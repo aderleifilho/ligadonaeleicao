5d3a82608142f37b891adef3c3144385670cda34
Guilherme Andrade <guilherme.andrade19@hotmail.com>
2014-08-22 22:31:32 -0300

    Commit Solution

---
+++ LigadoNoCandidato/Controllers/CandidatoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LigadoNoCandidato.Controllers
+{
+    public class CandidatoController : Controller
+    {
+        //
+        // GET: /Candidato/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Grafico()
+        {
+            return View();
+        }
+
+        public ActionResult Detalhe()
+        {
+            return View();
+        }
+
+    }
+}
